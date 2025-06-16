7909c7657fddc0f1b551a999e2440f4790dad3af
sa-savi <savi.toy@amkcambodia.com>
2025-06-16 18:11:11 +0700

    UpdateREADME

--- map_drive/beta/beta_fstab.sh
+++ map_drive/beta/beta_fstab.sh
@@ -2,9 +2,9 @@
 #
 ## Define all three fstab lines
 #fstab_entries=(
-#"//amkcambodia.com/amkdfs /media/%u/Collaboration-Q cifs credentials=/etc/smbcred/%u,uid=%U,gid=%G,prefixpath=Collaboration,sec=ntlmssp,vers=3.0,user,noauto 0 0"
-#"//amkcambodia.com/amkdfs /media/%u/Department-N cifs credentials=/etc/smbcred/%u,uid=%U,gid=%G,prefixpath=Dept_Doc/CIO/ITI,sec=ntlmssp,vers=3.0,user,noauto 0 0"
-#"//amkcambodia.com/amkdfs /media/%u/Home-H cifs credentials=/etc/smbcred/%u,uid=%U,gid=%G,prefixpath=StaffDoc/ITD/%u,sec=ntlmssp,vers=3.0,user,noauto 0 0"
+#"//amkcambodia.com/amkdfs /media/$logname/Collaboration-Q cifs credentials=/etc/smbcred/$logname,uid=%U,gid=%G,prefixpath=Collaboration,sec=ntlmssp,vers=3.0,user,noauto 0 0"
+#"//amkcambodia.com/amkdfs /media/$logname/Department-N cifs credentials=/etc/smbcred/$logname,uid=%U,gid=%G,prefixpath=Dept_Doc/CIO/ITI,sec=ntlmssp,vers=3.0,user,noauto 0 0"
+#"//amkcambodia.com/amkdfs /media/$logname/Home-H cifs credentials=/etc/smbcred/$logname,uid=%U,gid=%G,prefixpath=StaffDoc/ITD/%u,sec=ntlmssp,vers=3.0,user,noauto 0 0"
 #)
 #
 #for FSTAB_LINE in "${fstab_entries[@]}"; do
