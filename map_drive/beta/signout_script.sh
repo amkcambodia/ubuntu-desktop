78b058500893e11a85ef1d71f5b428bcf7d234d7
sa-savi <savi.toy@amkcambodia.com>
2025-06-19 14:46:48 +0700

    Update AD Change Password

--- map_drive/beta/signout_script.sh
+++ map_drive/beta/signout_script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-USERNAME=$(logname)
-
-mkdir -p ~/.config/systemd/$USERNAME
+mkdir -p ~/.config/systemd/user
 
 cat <<EOF > ~/.config/systemd/user/umount-dfs.service
 [Unit]
@@ -12,7 +10,7 @@ PartOf=graphical-session.target
 [Service]
 Type=oneshot
 RemainAfterExit=true
-ExecStop=/bin/bash "sudo /usr/local/bin/amk/umount-dfs.sh"
+ExecStop=sudo /usr/local/bin/amk/umount-dfs.sh
 EOF
 
 systemctl --user daemon-reload
