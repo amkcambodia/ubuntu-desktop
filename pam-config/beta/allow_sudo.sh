1f61f322103cbcd72f7f11bc61bf54c35fc06b54
sa-savi <savi.toy@amkcambodia.com>
2025-06-19 14:36:53 +0700

    Update AD Change Password

--- pam-config/beta/allow_sudo.sh
+++ pam-config/beta/allow_sudo.sh
@@ -6,8 +6,9 @@ FILE_PATH="/usr/local/bin/amk/mount-dfs.sh, /usr/local/bin/amk/umount-dfs.sh"
 
 
 # Create directory for sudoers.d if it doesn't exist
+sudo mkdir -p /etc/sudoers.d/
 sudo mkdir -p /etc/sudoers.d/amk
-sudo chmod 755 /etc/sudoers.d/amk
+sudo chmod 755 -R /etc/sudoers.d/
 
 # shellcheck disable=SC2016
 echo '%ubuntu-group ALL=(ALL) NOPASSWD: $FILE_PATH' | sudo tee /etc/sudoers.d/amk > /dev/null
