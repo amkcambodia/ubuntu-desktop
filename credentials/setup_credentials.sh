243b3a19c148bd38cf211d09132044bbdb3c596a
sa-savi <savi.toy@amkcambodia.com>
2025-06-19 10:39:14 +0700

    Update AD Change Password

--- credentials/setup_credentials.sh
+++ credentials/setup_credentials.sh
@@ -13,7 +13,7 @@ set -e
 # Run and install smbcred.sh
 sudo mkdir -p /bin/amk
 #sudo cp ./credentials/tasks/smbcred.sh /bin/amk/smbcred.sh
-sudo cp ./credentials/beta/beta_credential3.sh /bin/amk/smbcred.sh
+sudo cp ./credentials/template/credential3.sh /bin/amk/smbcred.sh
 sudo chmod 755 /bin/amk/smbcred.sh
 sudo chmod +x /bin/amk/smbcred.sh
 
